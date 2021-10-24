0b3b960ad061953a3cab6a44773dcd3b532753e2
Martin Stickley <mds1966@gmail.com>
2021-10-24 16:15:01 -0700

    Added a small program that adds 2 floating point numbers together and a gitignore so we dont commit to executables to git

---
+++ adder/adder.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+/* Headers */
+double add(double x, double y);
+
+int main()
+{
+    float first, second;
+    printf("Please enter the first number: \n");
+    scanf("%F", &first);
+    printf("Please enter the second number: \n");
+    scanf("%F", &second);
+
+    double add(double x, double y)
+    {
+        return x + y;
+    }
+
+    printf("The result of the addition is %F\n", add(first, second));
+
+    return 0;
+}
+
